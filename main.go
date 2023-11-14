abbc4b7e01084fc7c81376b2e8b74f4b849f9855
Paulo Soares <pauloxti@gmail.com>
2023-11-14 18:57:00 +0100

    panic on error

--- main.go
+++ main.go
@@ -23,7 +23,7 @@ func main() {
 	// load .env file
 	err := godotenv.Load()
 	if err != nil {
-		fmt.Println("Error loading .env file")
+		panic(err)
 	}
 	wsRPC := os.Getenv("WS_RPC")
 	limitOrderAddress := os.Getenv("LIMIT_ADDRESS")
@@ -36,13 +36,13 @@ func main() {
 
 	nineInchLimit, err := nineInchSpotLimitPLS.NewNineInchSpotLimitPLS(common.HexToAddress(limitOrderAddress), client)
 	if err != nil {
-		fmt.Println(err)
+		panic(err)
 	}
 
 	headers := make(chan *types.Header)
 	sub, err := client.SubscribeNewHead(context.Background(), headers)
 	if err != nil {
-		fmt.Println(err)
+		panic(err)
 	}
 
 	color.Green("Connected to %s", wsRPC)
@@ -52,28 +52,18 @@ func main() {
 		select {
 		case err := <-sub.Err():
 			fmt.Println(err)
-			client = getClient(wsRPC)
-			nineInchLimit, err = nineInchSpotLimitPLS.NewNineInchSpotLimitPLS(common.HexToAddress(limitOrderAddress), client)
-			if err != nil {
-				fmt.Println(err)
-			}
-
-			headers := make(chan *types.Header)
-			sub, err = client.SubscribeNewHead(context.Background(), headers)
-			if err != nil {
-				fmt.Println(err)
-			}
+			panic(err)
 		case header := <-headers:
 
 			block, err := client.BlockByHash(context.Background(), header.Hash())
 			if err != nil {
-				fmt.Println(err)
+				panic(err)
 			}
 			fmt.Println(color.YellowString("New Block Number"), color.CyanString("%d", block.Number().Uint64()))
 
 			shouldBeExecuted, orderId, err := nineInchLimit.CheckUpkeep(nil, []byte("0x"))
 			if err != nil {
-				fmt.Println(err)
+				panic(err)
 			}
 
 			present := lo.Contains(processedOrders, hex.EncodeToString(orderId))
@@ -90,7 +80,7 @@ func main() {
 
 					txHash, err := performUpkeep(client, nineInchLimit, orderId, privateKey)
 					if err != nil {
-						fmt.Println(err)
+						panic(err)
 					}
 					fmt.Println(color.YellowString("Tx Hash"), color.CyanString(txHash))
 					fmt.Println("")
@@ -104,16 +94,15 @@ func main() {
 func getClient(wsRPC string) *ethclient.Client {
 	var client *ethclient.Client
 	var err error
-	do := client == nil
-	for do {
-		// init eth client
-		color.Cyan("Connecting to %s", wsRPC)
-		client, err = ethclient.Dial(wsRPC)
-		if err != nil {
-			fmt.Println(color.RedString("Error connecting to %s", wsRPC))
-		}
-		do = client == nil
+
+	// init eth client
+	color.Cyan("Connecting to %s", wsRPC)
+	client, err = ethclient.Dial(wsRPC)
+	if err != nil {
+		fmt.Println(color.RedString("Error connecting to %s", wsRPC))
+		panic(err)
 	}
+
 	return client
 }
 
@@ -122,12 +111,12 @@ func performUpkeep(client *ethclient.Client, nineInchLimit *nineInchSpotLimitPLS
 	gasLimit := uint64(5300000) // in units
 	gasPrice, gasPriceErr := client.SuggestGasPrice(context.Background())
 	if gasPriceErr != nil {
-		fmt.Println(gasPriceErr)
+		panic(gasPriceErr)
 	}
 
 	privateKey, err := crypto.HexToECDSA(pk)
 	if err != nil {
-		fmt.Println(err)
+		panic(err)
 	}
 
 	publicKey := privateKey.Public()
@@ -141,7 +130,7 @@ func performUpkeep(client *ethclient.Client, nineInchLimit *nineInchSpotLimitPLS
 
 	tx, err := nineInchLimit.PerformUpkeep(transactor, performData)
 	if err != nil {
-		fmt.Println(err)
+		panic(err)
 	}
 
 	return tx.Hash().Hex(), nil
@@ -150,12 +139,12 @@ func performUpkeep(client *ethclient.Client, nineInchLimit *nineInchSpotLimitPLS
 func buildTransactor(client *ethclient.Client, privateKey *ecdsa.PrivateKey, fromAddress common.Address, value *big.Int, gasPrice *big.Int, gasLimit uint64) *bind.TransactOpts {
 	chainId, err := client.NetworkID(context.Background())
 	if err != nil {
-		fmt.Println(err)
+		panic(err)
 	}
 
 	transactor, err := bind.NewKeyedTransactorWithChainID(privateKey, chainId)
 	if err != nil {
-		fmt.Println(err)
+		panic(err)
 	}
 
 	transactor.Value = big.NewInt(0)
@@ -165,7 +154,7 @@ func buildTransactor(client *ethclient.Client, privateKey *ecdsa.PrivateKey, fro
 
 	nonce, err := client.PendingNonceAt(context.Background(), fromAddress)
 	if err != nil {
-		fmt.Println(err)
+		panic(err)
 	}
 
 	transactor.GasPrice = gasPrice
